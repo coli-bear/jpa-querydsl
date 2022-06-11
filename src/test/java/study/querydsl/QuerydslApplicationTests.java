28f86f337ae5a20f4b20b7ac68b22e00395f1112
박건태 <kj890720@icraft21.com>
2022-06-11 12:53:19 +0900

    configure Query DSL with gradle

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,38 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.domain.entity.Hello;
+import study.querydsl.domain.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello res = query.selectFrom(qHello)
+            .fetchOne();
+
+        assertThat(res).isEqualTo(hello);
+        assertThat(res.getId()).isEqualTo(hello.getId());
     }
 
 }
